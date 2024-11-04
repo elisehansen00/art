745401226cabe3f1b9b165b4f3aa184b471c720b
elisehansen00 <elmahans@ucsc.edu>
2024-11-04 12:51:22 -0800

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,25 @@
+// Author: Elise Hansen
+// Date: 10/29/2024
+
+// using jQuery to create a program
+
+// Challenge Button
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
+
+// Problem Button
+$("#problem").append("<button id='button-problem'>Make Special</button>");
+$("button-problem").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problem").toggleClass("special");
+});
+
+// Results Button
+$("#results").append("<button id='button-results'>Make Special</button>");
+$("button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
