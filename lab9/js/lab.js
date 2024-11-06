bfcc39ca39e59c5345cfb7729e16b5020fd36179
elisehansen00 <elmahans@ucsc.edu>
2024-11-05 17:30:57 -0800

    pleaseee

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,22 +4,22 @@
 // using jQuery to create a program
 
 // Challenge Button
-$("challenge").append("<button id='button-challenge'>Make Special</button>");
-$("button-challenge").click(function(){
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("challenge").toggleClass("special");
+    $("#challenge").toggleClass("special");
 });
 
 // Problem Button
-$("problem").append("<button id='button-problem'>Make Special</button>");
-$("button-problem").click(function(){
+$("#problem").append("<button id='button-problem'>Make Special</button>");
+$("#button-problem").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("problem").toggleClass("special");
+    $("#problem").toggleClass("special");
 });
 
 // Results Button
-$("results").append("<button id='button-results'>Make Special</button>");
-$("button-results").click(function(){
+$("#results").append("<button id='button-results'>Make Special</button>");
+$("#button-results").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("results").toggleClass("special");
+    $("#results").toggleClass("special");
 });
