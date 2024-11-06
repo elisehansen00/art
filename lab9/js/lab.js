481a2ecca1b27a8b274a4bc8f36e5ed860ecbadd
elisehansen00 <elmahans@ucsc.edu>
2024-11-05 17:39:19 -0800

    pretty please with a chair on top

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,18 +8,18 @@ $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 $("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
-});
+})
 
 // Problem Button
 $("#problem").append("<button id='button-problem'>Make Special</button>");
 $("#button-problem").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#problem").toggleClass("special");
-});
+})
 
 // Results Button
 $("#results").append("<button id='button-results'>Make Special</button>");
 $("#button-results").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#results").toggleClass("special");
-});
+})
