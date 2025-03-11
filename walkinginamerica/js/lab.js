5638b9d0ecec8ca4d77c10e0777e7736f7cec178
elisehansen00 <elmahans@ucsc.edu>
2025-03-10 20:44:45 -0700

    okay?

--- walkinginamerica/js/lab.js
+++ walkinginamerica/js/lab.js
@@ -19,3 +19,32 @@ function main() {
 
 // let's get this party started
 main();
+// Get the modal and the close button
+var modal = document.getElementById("imageModal");
+var modalImg = document.getElementById("modalImage");
+var captionText = document.getElementById("caption");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Get all photos in the gallery
+var photos = document.querySelectorAll('.photo');
+
+// When any photo is clicked, open the modal and display the clicked photo
+photos.forEach(function(photo) {
+  photo.addEventListener("click", function() {
+    modal.style.display = "flex";
+    modalImg.src = photo.src;
+    captionText.innerHTML = photo.alt;
+  });
+});
+
+// When the close button is clicked, close the modal
+closeBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+// Optional: Close the modal when clicking anywhere outside the modal
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
